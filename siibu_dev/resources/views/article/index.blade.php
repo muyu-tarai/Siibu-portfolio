58c6b60119214fa23bc38acca122eb688adc6c32
muyu-tarai <hello.tra.murai@gmail.com>
2023-08-07 16:40:56 +0900

    記事一覧ページ作成途中

--- siibu_dev/resources/views/article/index.blade.php
+++ siibu_dev/resources/views/article/index.blade.php
@@ -6,18 +6,22 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Siibu</title>
-  <link rel="stylesheet" href="/css/styles.css">
+  <link rel="stylesheet" href="/css/style.css">
 </head>
 
 <body>
   <header>
-    <nav class="my-navbar">
-      <a class="my-navbar-brand" href="/">Siibu</a>
+    <nav class="navbar">
+      <a class="header_title" href="#">Siibu</a>
+      <a class="header_mypage" href="#">myページ</a>
+      <a class="header_submit" href="#">投稿する</a>
     </nav>
   </header>
   <main>
     <div class="container">
-      <p>sushi</p>
+      <form action="#">
+        <input type="text" placeholder="記事を検索">
+      </form>
     </div>
   </main>
 </body>
