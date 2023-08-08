1da3ba40e8d027e5f9d6e36f4620511e0918b34c
muyu-tarai <hello.tra.murai@gmail.com>
2023-08-08 15:53:40 +0900

    記事一覧画面のサイドバーから

--- siibu_dev/resources/views/article/index.blade.php
+++ siibu_dev/resources/views/article/index.blade.php
@@ -6,24 +6,135 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Siibu</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
   <link rel="stylesheet" href="/css/style.css">
 </head>
 
 <body>
-  <header>
-    <nav class="navbar">
-      <a class="header_title" href="#">Siibu</a>
-      <a class="header_mypage" href="#">myページ</a>
-      <a class="header_submit" href="#">投稿する</a>
-    </nav>
-  </header>
+  <div class="header">
+    <header>
+      <nav class="navbar">
+        <a class="header_title x-large" href="#">Siibu</a>
+        <div class="header-right">
+          <a class="header_mypage medium" href="#">myページ</a>
+          <a class="header_submit medium" href="#">投稿する</a>
+        </div>
+      </nav>
+    </header>
+  </div>
   <main>
-    <div class="container">
+    <div class="index_main_contents">
       <form action="#">
-        <input type="text" placeholder="記事を検索">
+        <input type="text" placeholder="記事を検索" class="search-box">
       </form>
+      <div class="some-articles">
+        <article>
+          <div class="article-item">
+            <div class="small">
+              アカウント名
+            </div>
+            <div class="x-small">
+              2023年8月8日
+            </div>
+          </div>
+          <div class="article-item x-large">
+            HTMLが楽しすぎる
+          </div>
+          <div class="medium">
+            HTML
+          </div>
+        </article>
+        <article>
+          <div class="article-item">
+            <div class="small">
+              アカウント名
+            </div>
+            <div class="x-small">
+              2023年8月8日
+            </div>
+          </div>
+          <div class="article-item x-large">
+            HTMLが楽しすぎる
+          </div>
+          <div class="medium">
+            HTML
+          </div>
+        </article>
+        <article>
+          <div class="article-item">
+            <div class="small">
+              アカウント名
+            </div>
+            <div class="x-small">
+              2023年8月8日
+            </div>
+          </div>
+          <div class="article-item x-large">
+            HTMLが楽しすぎる
+          </div>
+          <div class="medium">
+            HTML
+          </div>
+        </article>
+        <article>
+          <div class="article-item">
+            <div class="small">
+              アカウント名
+            </div>
+            <div class="x-small">
+              2023年8月8日
+            </div>
+          </div>
+          <div class="article-item x-large">
+            HTMLが楽しすぎる
+          </div>
+          <div class="medium">
+            HTML
+          </div>
+        </article>
+        <article>
+          <div class="article-item">
+            <div class="small">
+              アカウント名
+            </div>
+            <div class="x-small">
+              2023年8月8日
+            </div>
+          </div>
+          <div class="article-item x-large">
+            HTMLが楽しすぎる
+          </div>
+          <div class="medium">
+            HTML
+          </div>
+        </article>
+        <div class="medium changing-page">
+          1/3 >
+        </div>
+      </div>
+    </div>
+    <div class="index-sidebar medium">
+      <div>
+        タグ
+      </div>
+      <div>
+        IT基礎知識
+      </div>
+      <div>
+        HTML
+      </div>
     </div>
   </main>
+  <div class="footer">
+    <footer>
+      <div class="medium">
+        Siibu
+      </div>
+      <div class="x-small">
+        © 2023 Siib Inc
+      </div>
+    </footer>
+  </div>
 </body>
 
 </html>
