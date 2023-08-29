da8adb42b6f25c637d4e671ecd5173f6d2b2b580
muyu-tarai <hello.tra.murai@gmail.com>
2023-08-29 15:47:07 +0900

    test

--- siibu_dev/resources/views/article_list/item.blade.php
+++ siibu_dev/resources/views/article_list/item.blade.php
@@ -1,1 +1,21 @@
-<p>aaa</p>
+<article>
+  <div class="article-item">
+    <div class="small">
+      {{ $article->user_name }}
+    </div>
+    <div class="x-small">
+      {{ $article->article_created_at }}
+    </div>
+  </div>
+  <div class="article-item x-large">
+    <a href="/article_list/item">{{ $article->title }}</a>
+  </div>
+  <div class="medium">
+    @foreach($relatedTags[$article->article_id] as $relatedTag)
+    {{ $relatedTag }}
+    @endforeach
+  </div>
+  <div class="medium">
+    <i class="fa-solid fa-heart" style="color: #ff5757;"></i>
+    {{ $article->number_of_likes }}
+  </div>
