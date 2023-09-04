cb5cbf54868602f73cdaeaad61ffdea9907a7ae4
muyu-tarai <hello.tra.murai@gmail.com>
2023-09-04 16:34:19 +0900

    削除機能完成

--- siibu_dev/app/Http/Controllers/ArticleDeleteController.php
+++ siibu_dev/app/Http/Controllers/ArticleDeleteController.php
@@ -3,16 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ArticleDeleteController extends Controller
 {
-    public function articleDelete()
+    public function articleDelete($articleId)
     {
-        return view('mypage/article_delete/articleDelete');
+        return view('mypage/article_delete/articleDelete', ['article_id'=>$articleId]);
     }
 
-    public function articleDeleteComplete()
+    public function articleDeleteComplete($articleId)
     {
-        return view('mypage/articleDeleteComplete');
+        DB::table('articles')
+        ->where('id', $articleId)
+            ->delete();
+
+        return view('mypage/article_delete/articleDeleteComplete');
     }
 }
--- siibu_dev/resources/views/mypage/article_delete/articleDelete.blade.php
+++ siibu_dev/resources/views/mypage/article_delete/articleDelete.blade.php
@@ -3,6 +3,8 @@
     <div>
       本当に記事を削除してもよろしいですか？
     </div>
-    <form action=""></form>
+    <form action="/mypage/article_delete/{{ $article_id }}/complete" method="GET">
+      <input type="submit">
+    </form>
   </div>
 </x-layout>
